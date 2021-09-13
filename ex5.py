9f1ecdcdcbaf90a2098f0fa308e0adcd321717c9
Your Name <malo2b>
2021-09-13 16:42:02 +0200

    commit

--- ex5.py
+++ ex5.py
@@ -18,3 +18,60 @@ def present(liste:list[int],element:int) -> bool:
             res = True
         i += 1
     return res
+
+def test_present(present: callable):
+    if not present([], 10):
+        print("SUCCES : test liste vide")
+    else:
+        print("ECHEC : test liste vide")
+
+    print("\nTEST DEBUT")
+    if present([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1):
+        print("SUCCES TEST DEBUT")
+    else:
+        print("ECHEC TEST DEBUT")
+    print("TEST FIN\n")
+    if present([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10):
+        print("SUCCES TEST FIN")
+    else:
+        print("ECHEC TEST FIN")
+    print("TEST MILIEU")
+    if present([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5):
+        print("SUCCES TEST MILIEU")
+    else:
+        print("ECHEC TEST MILIEU")
+    if not present([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11):
+        print("SUCCES TEST absent")
+    else:
+        print("ECHEC TEST absent")
+
+# VERSION 1
+
+
+def present1(lst, e):
+    for i in range(0, len(lst), 1):
+        if (lst[i] == e):
+            return(True)
+        else:
+            return (False)
+    return (False)
+
+def present2 (lst, e) :
+    b=True
+    for i in range (0, len(lst), 1) :
+        if (lst[i] == e) :
+            b=True
+        else :
+            b=False
+    return (b)
+
+
+def present3 (lst, e) :
+    b=True
+    for i in range (0, len(lst), 1) :
+        return (lst[i] == e)
+
+
+
+
+test_present(present3)
